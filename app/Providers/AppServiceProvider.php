190f8c7330f6461b22847574221cdbedd42eb61a
bakanyaka <bakayaroshka@gmail.com>
2018-03-29 16:18:52 +0300

    Replace Larevel Debug Bar with Clockwork

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //\Debugbar::disable();
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
